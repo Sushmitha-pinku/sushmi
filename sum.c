dab908bfb64d41856146f132a5c75aaf2807cf7c
shwetha-ai <shwethakc959@gmail.com>
2024-10-20 17:51:10 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
  void main()
  {
- 	printf(" hi welcome to c programmiz\n");
+  printf(" hi welcome to c programmiz\n");
+ 	a=10,b=20,sum;
+  sum=a+b;
+  printf(" sum=%d",&sum);
  }
